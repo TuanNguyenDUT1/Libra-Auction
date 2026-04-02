5c74a6f8a166fccbd4e180eb676cba910d0f9c70
Guen Nhat King <GuenNhatKing@gmail.com>
2026-04-02 08:19:29 +0700

    fix: replace anchor tag with Link component for Show More button

--- Frontend/libra_auction/components/show_more_btn.tsx
+++ Frontend/libra_auction/components/show_more_btn.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
+
 export default function ShowMoreBtn() {
-    return <a href="/auctions" className="bg-white text-(--primary-color) border border-(--primary-color) text-xl px-6 py-2">
+    return <Link href="/auctions" className="bg-white text-(--primary-color) border border-(--primary-color) text-xl px-6 py-2">
         Show More
-    </a>
+    </Link>
 }
